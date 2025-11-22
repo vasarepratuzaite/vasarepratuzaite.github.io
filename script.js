44785a38d8087f380f7b567afebf21483bd6c739
Vasarė Pratuzaitė <122165883+vasarepratuzaite@users.noreply.github.com>
2025-11-22 23:28:14 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop - 20,
+            behavior: "smooth"
+        });
+    });
+});
